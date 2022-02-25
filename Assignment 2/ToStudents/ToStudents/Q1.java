ff8c98cfa7be4f5de028768f0306c571f8f482e1
karaebest <kara.best@mail.mcgill.ca>
2022-02-25 17:02:03 -0500

    Update Q1.java

--- Assignment 2/ToStudents/ToStudents/Q1.java
+++ Assignment 2/ToStudents/ToStudents/Q1.java
@@ -3,10 +3,12 @@ import java.util.*;
 public class Q1 {
 	public static int[] last = new int[2];
 	public static int initialNumOfBalls = 0;
+	public static HashSet<int[]> nilSpaces;
 
 	public static HashSet<int[]> positions(String[][] board){ //to insert initial ball positions into hashset
 
 		HashSet<int[]> balls = new HashSet<>();
+
 
 		int i=0;
 
@@ -28,6 +30,7 @@ public class Q1 {
 		ArrayList<int[]> jump = new ArrayList<>();
 		int i = pos[0];
 		int j = pos[1];
+		
 
 
 		if(i+2<5 && (!((i+2==4) && (j<3 || j>5))) && balls.contains(new int[] {i+1, j})){ //space above
@@ -48,50 +51,7 @@ public class Q1 {
 		
 	}
 
-	public static ArrayList<HashSet<int[]>> amend(int[] delete, int[] oldP, int[] newP, HashSet<int[]> inPlay, HashSet<int[]> fixed){
-
-		int[] diff = new int[] {-1,1};
-		int[] del, o, n;
-		int v, h; 
-		int d=0;
-		int k=0;
-
-		while(k<4){
-			v = (k%2==0)?1:0;
-			h = (v==1)?0:1;
-			del = new int[] {delete[0]+2*v*diff[d], delete[1]+2*h*diff[d]}; //+- 2 horz and vert from deleted space
-			o = new int[] {oldP[0]+2*v*diff[d], oldP[1]+2*h*diff[d]};//+- 2 horz and vert from old space
-			n = new int[] {newP[0]+v*diff[d], newP[1]+h*diff[d++]};//+- 1 horz and vert from new space
-			if(fixed.contains(del) && !jumps(del, inPlay, fixed).isEmpty()){
-				fixed.remove(del); inPlay.add(del);
-			}if(fixed.contains(o) && !jumps(o, inPlay, fixed).isEmpty()){
-				fixed.remove(o); inPlay.add(o);
-			}if(fixed.contains(del) && !jumps(del, inPlay, fixed).isEmpty()){
-				fixed.remove(del); inPlay.add(del);
-			}
-
-		}
-		// for(int k=0; k<4; k++){
-		// 	if(k<2){ //only check fixed since moveBall() amends inPlay
-		// 		dV = new int[] {delete[0]+diff[k], delete[1]};
-		// 		dH = new int[] {delete[0], delete[1]+diff[k]};
-		// 		oldV = new int[] {oldP[0]+diff[k], oldP[1]};
-		// 		oldH = new int[] {oldP[0], oldP[1]+diff[k]};
-		// 		if(fixed.contains(dV)){
-		// 			if(!jumps(dV, inPlay, fixed).isEmpty()){
-		// 				fixed.remove(dV); inPlay.add(dV);
-		// 			}
-		// 			if(!jumps(dH, inPlay, fixed).isEmpty()){
-
-		// 			}
-		// 		}
-		// 	}else{
-
-		// 	}
-		// }
-
-	}
-
+	
 
 	public static void moveBall(HashSet<int[]> balls, int[] sol, int[] toDelete, int[] oldPos){
 
@@ -101,40 +61,35 @@ public class Q1 {
 			newPos[0] = (toDelete[0]-oldPos[0]==0)?toDelete[0]:(2*toDelete[0]-oldPos[0]);
 			newPos[1] = (toDelete[1]-oldPos[1]==0)?toDelete[1]:(2*toDelete[1]-oldPos[1]);
 			//delete jumped over, change index of moved ball
-			if(inPlay.contains(toDelete)){
-				inPlay.remove(toDelete);
-			}else{
-				fixed.remove(toDelete);
-			}
-			inPlay.remove(oldPos);
-			ArrayList<int[]> newJumps = jumps(newPos, inPlay, fixed);
-			if(newJumps.isEmpty()){
-				fixed.add(newPos);
-			}else{
-				inPlay.add(newPos);
-			}
-			//call jumps on new position and add to fixed if none
-			//call jumps on old position, check fixed.contains(jumps[i]), if true call jumps on jumps[i] and switch from fixed to positions if there are possible jumps
+			balls.remove(toDelete);
+			balls.remove(oldPos);
+			balls.add(newPos);
+			
 		}
 
-		sol[0] = inPlay.size()+fixed.size(); 
+		sol[0] = balls.size(); 
 		if(sol[0]==1){ //no better solution for the board
 			A2_Q1.last = sol;
 			return;
 		}
 		
-		//if positions.size()==0, compare to previous solution last and replace if better, do nothing o/w
-		if(inPlay.size()==0){
-			A2_Q1.last = last[0]<=sol[0]?last:sol;
+		Iterator b = balls.iterator();
+		int j = 1;
+
+		while(b.hasNext()){ //iterate through balls
+			int[] temp = b.next();
+			ArrayList<int[]> jumps = jumps(temp, balls);
+			j = jumps.isEmpty()?j+1:j;
+
+			Iterator jp = jumps.iterator();
+			while(jp.hasNext()){ //iterate through ball[i] jumps
+				moveBall(balls, sol, jp.next(), temp);
+			}
 		}
 
-		//iterate through positions
-			//if jumps is empty
-				//move into fixed
-				//delete from positions 
-				//if positions.size()==0, compare to previous solution last and replace if better, do nothing o/w
-			//iterate through possible jumps
-				//call moveBall(positions, fixed, sol, jumps[i], positions[i])
+		if(j==balls.size()){
+			A2_Q1.last = last[0]<=sol[0]?last:sol;
+		}
 	
 	}
 	
